d9c269cbe269868ab5561e69a87594de14df74b9
Amir <amirbakarov@gmail.com>
2023-07-05 13:57:25 -0400

    initial commit

---
+++ src/blueprints/thread.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, url_for, flash, redirect, send_from_directory
+from database.database import get_thread_by_id, add_post
+from utils.data_parsers import parse_content, parse_request_images
+from utils.data_rendering import render_thread
+
+from globals import RELATIVE_UPLOADS_DIR
+
+thread_bp = Blueprint('thread', __name__)
+
+@thread_bp.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(RELATIVE_UPLOADS_DIR, filename)
+
+@thread_bp.route('/thread/<thread_id>', methods=['GET', 'POST'])
+def thread(thread_id):
+    if request.method == 'POST':
+        content = parse_content(request.form['content'])
+        images = parse_request_images(request.files.getlist('image'))
+
+        if content is None:
+            flash('Content is required!')
+            return redirect(url_for('thread.thread', thread_id=thread_id))
+
+        new_thread = False
+        sender_ip = None
+        title = None
+        add_post(new_thread, content, sender_ip, images, thread_id, title)
+
+        return redirect(url_for('thread.thread', thread_id=thread_id))
+    
+    thread = get_thread_by_id(thread_id)
+    rendered_thread = render_thread(thread)
+    
+    return render_template("thread.html", thread=rendered_thread)
+
---
+++ src/utils/data_rendering.py
@@ -0,0 +1,45 @@
+import markdown
+
+from database.database import get_post_by_id
+
+def render_threads(threads):
+    rendered_threads = []
+    for thread in threads:
+        rendered_thread = render_thread(thread)
+        rendered_threads.append(rendered_thread)
+    return rendered_threads
+
+def render_thread(thread):
+    rendered_thread = []
+    for post in thread:
+        rendered_post = render_post(post)
+        rendered_thread.append(rendered_post)
+    return rendered_thread
+
+def render_post(post):
+    rendered_post = post
+    rendered_lines = []
+    for line in rendered_post['post_content'].split('<br>'):
+        rendered_line = {}
+        if line.startswith('>>'):
+            rendered_line['text'] = line
+            rendered_line['type'] = 'quote'
+            quoted_post_id =  line.split('>>')[1]
+            quoted_post = get_post_by_id(quoted_post_id)
+            rendered_line['quoted_post'] = None
+            if quoted_post is not None:
+                rendered_line['quoted_post'] = render_post(quoted_post)
+        else:
+            rendered_line['text'] = markdown.markdown(line)
+            rendered_line['type'] = 'text'
+        rendered_lines.append(rendered_line)
+    rendered_post['rendered_content'] = rendered_lines
+    if post['images']:
+        rendered_images = []
+        for image in post['images']:
+            rendered_image = image
+            rendered_image['download_link'] = str(image['id']) + image['extension']
+            rendered_images.append(rendered_image)
+        rendered_post['images'] = rendered_images
+    return rendered_post
+    
